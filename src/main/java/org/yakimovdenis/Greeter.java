ce2512b2de5d86b676c696863f345bbddd414e42
Gvo3d <denis.yakimov89@gmail.com>
2017-09-18 16:50:21 +0300

    0.2 - All working but tests

--- src/main/java/org/yakimovdenis/Greeter.java
+++ src/main/java/org/yakimovdenis/Greeter.java
@@ -1,11 +1,14 @@
 package org.yakimovdenis;
 
-
-import org.yaml.snakeyaml.Yaml;
+import org.apache.commons.lang.LocaleUtils;
+import org.apache.commons.lang.StringUtils;
 
 import java.io.*;
+import java.text.SimpleDateFormat;
 import java.util.Locale;
 import java.util.Properties;
+import java.util.TimeZone;
+import java.util.concurrent.TimeUnit;
 
 public class Greeter {
     public static void main(String[] args) {
@@ -16,7 +19,50 @@ public class Greeter {
         }
         if (args.length == 0) {
             System.out.println(properties.getProperty("error.emptyArgs"));
+            System.exit(1);
         }
+
+        String cityName = StringUtils.capitalize(StringUtils.lowerCase(args[0]));
+        String timeZone = null;
+        int tzTime = 0;
+        if (args.length > 1) {
+            timeZone = args[1];
+        } else {
+            String[] ids = TimeZone.getAvailableIDs();
+            String comparsionCity = cityName.replace(" ", "_");
+            for (String id : ids) {
+                if (id.contains(comparsionCity)) {
+                    tzTime = Math.toIntExact(TimeUnit.MILLISECONDS.toHours(TimeZone.getTimeZone(id).getRawOffset()));
+                }
+            }
+        }
+            SimpleDateFormat sdf = new SimpleDateFormat("HH");
+            if (timeZone != null) {
+                try {
+                    TimeZone tzObject = TimeZone.getTimeZone(timeZone);
+                    sdf.setTimeZone(tzObject);
+                } catch (Exception e) {
+                    System.out.println(properties.getProperty("error.wrongTimeZone") + " : " + timeZone);
+                }
+            } else {
+                sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+            }
+            StringBuilder builder = new StringBuilder();
+            int hours = Integer.parseInt(sdf.format(System.currentTimeMillis()));
+            hours = hours +tzTime;
+            if (hours >= 6 && hours < 9) {
+                builder.append(properties.getProperty("result.morning"));
+            } else if (hours >= 9 && hours < 19) {
+                builder.append(properties.getProperty("result.day"));
+            } else if (hours >= 19 && hours < 23) {
+                builder.append(properties.getProperty("result.evening"));
+            } else if (hours >= 23 || hours < 6) {
+                builder.append(properties.getProperty("result.night"));
+            }
+            builder.append(", ");
+            builder.append(cityName);
+            builder.append("!");
+        System.out.println(builder.toString());
     }
 
     private static Properties initializeProps() {
