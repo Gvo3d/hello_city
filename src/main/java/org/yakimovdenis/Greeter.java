3e14ee1108c704bdf816d2117f71455015772469
Gvo3d <denis.yakimov89@gmail.com>
2017-09-18 17:45:25 +0300

    0.6 - logging

--- src/main/java/org/yakimovdenis/Greeter.java
+++ src/main/java/org/yakimovdenis/Greeter.java
@@ -2,6 +2,8 @@ package org.yakimovdenis;
 
 import org.apache.commons.lang.LocaleUtils;
 import org.apache.commons.lang.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.*;
 import java.text.SimpleDateFormat;
@@ -11,14 +13,18 @@ import java.util.TimeZone;
 import java.util.concurrent.TimeUnit;
 
 public class Greeter {
+    private final static Logger slf4jLogger = LoggerFactory.getLogger(Greeter.class);
+
     public static void main(String[] args) {
         Properties properties = initializeProps();
         if (null == properties) {
-            System.out.println("Internationalization properties file is not found");
+            String error = "Internationalization properties file is not found";
+            System.err.println(error);
+            slf4jLogger.error(error);
             System.exit(1);
         }
         if (args.length == 0) {
-            System.out.println(properties.getProperty("error.emptyArgs"));
+            slf4jLogger.error(properties.getProperty("error.emptyArgs"));
             System.exit(1);
         }
 
@@ -42,7 +48,8 @@ public class Greeter {
                     TimeZone tzObject = TimeZone.getTimeZone(timeZone);
                     sdf.setTimeZone(tzObject);
                 } catch (Exception e) {
-                    System.out.println(properties.getProperty("error.wrongTimeZone") + " : " + timeZone);
+                    slf4jLogger.error(properties.getProperty("error.wrongTimeZone") + " : " + timeZone);
+                    sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
                 }
             } else {
                 sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
@@ -76,18 +83,18 @@ public class Greeter {
         } catch (IOException ex) {
             lang = "properties_en.properties";
             reader = Greeter.class.getClassLoader().getResourceAsStream(lang);
-            System.out.println("Internationalization properties file is not found: " + lang);
+            slf4jLogger.error("Internationalization properties file is not found: " + lang);
             System.exit(1);
             try {
                 prop.load(reader);
             } catch (IOException ex1) {
-                System.out.println("Something went wrong with properties: " + lang);
+                slf4jLogger.error("Something went wrong with properties: " + lang);
             } finally {
                 if (reader != null) {
                     try {
                         reader.close();
                     } catch (IOException ioe) {
-                        System.out.println("Something went wrong when closing " + lang);
+                        slf4jLogger.error("Something went wrong when closing " + lang);
                     }
                 }
             }
