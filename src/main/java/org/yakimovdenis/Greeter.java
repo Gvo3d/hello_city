202dcde6b5dfe1d94a0a6789ca83f8b56abbfc5f
Gvo3d <denis.yakimov89@gmail.com>
2017-09-19 19:22:09 +0300

    0.7 - ResourceBundle

--- src/main/java/org/yakimovdenis/Greeter.java
+++ src/main/java/org/yakimovdenis/Greeter.java
@@ -7,98 +7,80 @@ import org.slf4j.LoggerFactory;
 
 import java.io.*;
 import java.text.SimpleDateFormat;
-import java.util.Locale;
-import java.util.Properties;
-import java.util.TimeZone;
+import java.util.*;
 import java.util.concurrent.TimeUnit;
 
 public class Greeter {
     private final static Logger slf4jLogger = LoggerFactory.getLogger(Greeter.class);
 
     public static void main(String[] args) {
-        Properties properties = initializeProps();
-        if (null == properties) {
-            String error = "Internationalization properties file is not found";
-            System.err.println(error);
-            slf4jLogger.error(error);
-            System.exit(1);
-        }
+        ResourceBundle resources = getResourceBundleInstance();
+        System.out.println("res: "+resources.getLocale());
+
         if (args.length == 0) {
-            slf4jLogger.error(properties.getProperty("error.emptyArgs"));
+            slf4jLogger.error(resources.getString("error.emptyArgs"));
             System.exit(1);
         }
 
         String cityName = StringUtils.capitalize(StringUtils.lowerCase(args[0]));
-        String timeZone = null;
-        int tzTime = 0;
+        int timeZoneOffset;
         if (args.length > 1) {
-            timeZone = args[1];
+            timeZoneOffset = getTimeZoneDelta(args[1], cityName);
         } else {
-            String[] ids = TimeZone.getAvailableIDs();
-            String comparsionCity = cityName.replace(" ", "_");
-            for (String id : ids) {
-                if (id.contains(comparsionCity)) {
-                    tzTime = Math.toIntExact(TimeUnit.MILLISECONDS.toHours(TimeZone.getTimeZone(id).getRawOffset()));
-                }
-            }
+            timeZoneOffset = getTimeZoneDelta(null, cityName);
         }
-            SimpleDateFormat sdf = new SimpleDateFormat("HH");
-            if (timeZone != null) {
-                try {
-                    TimeZone tzObject = TimeZone.getTimeZone(timeZone);
-                    sdf.setTimeZone(tzObject);
-                } catch (Exception e) {
-                    slf4jLogger.error(properties.getProperty("error.wrongTimeZone") + " : " + timeZone);
-                    sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
-                }
-            } else {
-                sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
-            }
-            StringBuilder builder = new StringBuilder();
-            int hours = Integer.parseInt(sdf.format(System.currentTimeMillis()));
-            hours = hours +tzTime;
-            if (hours >= 6 && hours < 9) {
-                builder.append(properties.getProperty("result.morning"));
-            } else if (hours >= 9 && hours < 19) {
-                builder.append(properties.getProperty("result.day"));
-            } else if (hours >= 19 && hours < 23) {
-                builder.append(properties.getProperty("result.evening"));
-            } else if (hours >= 23 || hours < 6) {
-                builder.append(properties.getProperty("result.night"));
-            }
-            builder.append(", ");
-            builder.append(cityName);
-            builder.append("!");
+
+        SimpleDateFormat sdf = new SimpleDateFormat("HH");
+        sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+        StringBuilder builder = new StringBuilder();
+        int hours = Integer.parseInt(sdf.format(System.currentTimeMillis()));
+        hours = hours + timeZoneOffset;
+
+        if (hours >= 6 && hours < 9) {
+            builder.append(resources.getString("result.morning"));
+        } else if (hours >= 9 && hours < 19) {
+            builder.append(resources.getString("result.day"));
+        } else if (hours >= 19 && hours < 23) {
+            builder.append(resources.getString("result.evening"));
+        } else if (hours >= 23 || hours < 6) {
+            builder.append(resources.getString("result.night"));
+        }
+        builder.append(", ");
+        builder.append(cityName);
+        builder.append("!");
         System.out.println(builder.toString());
     }
 
-    private static Properties initializeProps() {
-        Locale currentLocale = Locale.getDefault();
-        Properties prop = new Properties();
-        String lang = "properties_" + currentLocale.getLanguage() + ".properties";
-        InputStream reader;
-        reader = Greeter.class.getClassLoader().getResourceAsStream(lang);
-        try {
-            prop.load(reader);
-        } catch (IOException ex) {
-            lang = "properties_en.properties";
-            reader = Greeter.class.getClassLoader().getResourceAsStream(lang);
-            slf4jLogger.error("Internationalization properties file is not found: " + lang);
-            System.exit(1);
-            try {
-                prop.load(reader);
-            } catch (IOException ex1) {
-                slf4jLogger.error("Something went wrong with properties: " + lang);
-            } finally {
-                if (reader != null) {
-                    try {
-                        reader.close();
-                    } catch (IOException ioe) {
-                        slf4jLogger.error("Something went wrong when closing " + lang);
-                    }
+    private static int getTimeZoneDelta(String timeZoneString, String cityNameString) {
+        int result = 0;
+        if (null == timeZoneString) {
+            String[] ids = TimeZone.getAvailableIDs();
+            String comparsionCity = cityNameString.replace(" ", "_");
+            for (String id : ids) {
+                if (id.contains(comparsionCity)) {
+                    result = Math.toIntExact(TimeUnit.MILLISECONDS.toHours(TimeZone.getTimeZone(id).getRawOffset()));
                 }
             }
+        } else {
+            result = Math.toIntExact(TimeUnit.MILLISECONDS.toHours(TimeZone.getTimeZone(timeZoneString).getRawOffset()));
         }
-        return prop;
+        return result;
+    }
+
+    private static ResourceBundle getResourceBundleInstance() {
+        Locale currLocale = Locale.getDefault();
+        ResourceBundle resources = null;
+        String baseName = "properties";
+        try {
+            resources = ResourceBundle.getBundle(baseName, currLocale);
+        } catch (MissingResourceException e) {
+            try {
+                resources = ResourceBundle.getBundle(baseName, Locale.ENGLISH);
+            } catch (MissingResourceException e1) {
+                slf4jLogger.error("Internationalization properties file is not found: " + baseName);
+                System.exit(1);
+            }
+        }
+        return resources;
     }
 }
